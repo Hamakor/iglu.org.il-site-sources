7842ad03d7210ca4f0787ea5fae3be61ed49bd10
Shlomi Fish <shlomif@shlomifish.org>
2020-07-10 16:45:59 +0300

    avoid a makefile without includes

--- latemp-based-site/gen-helpers.pl
+++ latemp-based-site/gen-helpers.pl
@@ -3,22 +3,24 @@
 use strict;
 use warnings;
 
-use HTML::Latemp::GenMakeHelpers;
+use HTML::Latemp::GenMakeHelpers ();
 
-my $generator =
-    HTML::Latemp::GenMakeHelpers->new(
-        'hosts' =>
-        [
-            map
-            {
-                +{ 'id' => $_, 'source_dir' => "src/$_",
-                   'dest_dir' => "\$(ALL_DEST_BASE)/$_",
-               },
-            }
-            (qw(common iglu))
-        ]
-    );
+use Path::Tiny qw/ path tempdir tempfile cwd /;
+
+my $generator = HTML::Latemp::GenMakeHelpers->new(
+    'hosts' => [
+        map {
+            +{
+                'id'         => $_,
+                'source_dir' => "src/$_",
+                'dest_dir'   => "\$(ALL_DEST_BASE)/$_",
+                },
+        } (qw(common iglu))
+    ]
+);
 
 $generator->process_all();
+
+path("Makefile")->spew_utf8("include lib/make/main.mak\n");
 
 1;
